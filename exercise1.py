3ce15822c76f22e70dd5b1db71e8050c19fda4d0
izamBSU <izamahamed204@gmail.com>
2025-03-22 13:57:46 +0400

    exercise1.py

---
+++ exercise1.py
@@ -0,0 +1,9 @@
+#assigning variables to strings
+word1 = "coding"
+#assigning variables to strings
+word2 = "is"
+#assigning variables to strings
+word3 = "cool"
+
+# Use string concatenation to combine the variables and print the phrase
+print(word1 + " " + word2 + " " + word3)
